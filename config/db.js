80a8972d97c04ae6dc990ce87e74e59c6fbe639c
sourdi <sourdi.tn@icloud.com>
2024-08-28 19:43:52 +0100

    db.js

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const connection = mongoose.createConnection('mongodb+srv://sourditn:w58kwehvHZroQGjT@sourdi.dymm7.mongodb.net/yourDatabaseName', {
+const connection = mongoose.createConnection('mongodb+srv://sourditn:w58kwehvHZroQGjT@sourdi.dymm7.mongodb.net/?retryWrites=true&w=majority&appName=sourdi', {
   connectTimeoutMS: 0,  // No time limit for initial connection
   socketTimeoutMS: 0,   // No time limit for ongoing socket communication
 })
