32ea6d66af299d095a83f03fc65c31130c77ecf3
sourdi <sourdi.tn@icloud.com>
2024-08-28 18:52:42 +0100

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,11 +1,14 @@
 const mongoose = require('mongoose');
 
-
-const connection = mongoose.createConnection('mongodb+srv://sourditn:w58kwehvHZroQGjT@sourdi.dymm7.mongodb.net/?retryWrites=true&w=majority&appName=sourdi').on('open',() => {
-    console.log('MongoDb Connected');
-}).on('error',() => {
-    console.log('MongoDb connection error')
+const connection = mongoose.createConnection('mongodb+srv://sourditn:w58kwehvHZroQGjT@sourdi.dymm7.mongodb.net/yourDatabaseName', {
+  connectTimeoutMS: 0,  // No time limit for initial connection
+  socketTimeoutMS: 0,   // No time limit for ongoing socket communication
 })
+  .on('open', () => {
+    console.log('MongoDB Connected');
+  })
+  .on('error', (err) => {
+    console.error('MongoDB connection error:', err);
+  });
 
-
-module.exports =  connection;
+module.exports = connection;
