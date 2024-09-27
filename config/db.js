580d0c6b4ec8b7e2a6d14ab4094f33cce720ba29
sourdi <sourdi.tn@icloud.com>
2024-09-27 20:54:15 +0100

    Prototype-ready

--- config/db.js
+++ config/db.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
+const connectionURI = process.env.MONGODB_URI;
 
-const connection = mongoose.createConnection('mongodb://localhost:27017', {
+
+const connection = mongoose.createConnection(connectionURI, {
   connectTimeoutMS: 0,  // No time limit for initial connection
   socketTimeoutMS: 0,   // No time limit for ongoing socket communication
 })
--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const connection = require('./config/db');
 
 
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 
 // setInterval(() => {
